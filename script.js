4f1fc88dae66268e3ee1c8a0819c2acc5c9089b7
kalki39 <120099754+kalki39@users.noreply.github.com>
2023-02-22 01:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,32 +2,35 @@ const getSumBtn = document.createElement("button");
 const table = document.getElementById('table');
 // console.log(table.)
 getSumBtn.append("Get Total Price");
+const price = document.querySelectorAll(".price")
 
 // getSumBtn.append(table);
 document.body.appendChild(getSumBtn);
 
-const price=document.querySelectorAll(".price")
+
 
 // let priceArr
-let sum=0
-price.forEach((ele) => sum+=Number(ele.innerText))
-console.log(sum)
-let tr=table.insertRow(-1);
-	let td1=tr.insertCell(0);
-	let td2=tr.insertCell(1);
-    
-    td2.id='ans';
-	//  tr=`<td>to
+
+let tr = table.insertRow(-1);
+let td1 = tr.insertCell(0);
+let td2 = tr.insertCell(1);
+
+td2.id = 'ans';
+//  tr=`<td>to
 // getSumBtn.append(priceArr);
 const getSum = () => {
-//Add your code here
-	// let totalSum=priceArr.reduce((p,c) => p+c)
-	// tal</td><td>${sum}</td>`
-    td1.innerText="Total"
-    td2.innerText=sum
-//   tr.appendChild(td1);
-//   tr.appendChild(td2);
-//   table.append(tr);
+    //Add your code here
+    // let totalSum=priceArr.reduce((p,c) => p+c)
+    // tal</td><td>${sum}</td>`
+    //  price = document.querySelectorAll(".price")
+    let sum = 0
+    price.forEach((ele) => sum += Number(ele.innerText))
+    console.log(sum)
+    td1.innerText = "Total"
+    td2.innerText = sum
+    //   tr.appendChild(td1);
+    //   tr.appendChild(td2);
+    //   table.append(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
