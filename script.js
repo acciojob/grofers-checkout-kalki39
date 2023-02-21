c970215ebfe68be050065271546120a8e3c6e250
kalki39 <120099754+kalki39@users.noreply.github.com>
2023-02-21 22:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const price=document.querySelectorAll(".price").innerText
+
 const getSum = () => {
 //Add your code here
-  
+  document.body.appendChild(price);
 };
 
 getSumBtn.addEventListener("click", getSum);
